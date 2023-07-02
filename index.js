6356fa4c1abc2422ea37f97b27ac05cf5e94e1de
bhss <bruno.souza@matera.com>
2023-07-02 17:25:27 -0300

    Commit inicial;

---
+++ index.js
@@ -0,0 +1,33 @@
+const cron = require("node-cron");
+const axios = require("axios");
+
+cron.schedule("0 */10 * * * *", function () {
+  console.log("---------------------");
+  console.log("Checking every 10 minutes");
+
+  axios.get("https://digital-menu-api-0.onrender.com")
+    .then(function (response) {
+        console.log("Backend OK");
+    })
+    .catch(function (error) {
+        if(error.response.status == 401) {
+            console.log("Backend OK");
+        } else {
+            console.log(error);
+        }
+        
+    })
+    .finally(function () {
+    });
+
+  axios.get("https://digital-menu-backoffice-0.onrender.com")
+    .then(function (response) {
+        console.log("Frontend OK");
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(function () {
+    });
+
+});
